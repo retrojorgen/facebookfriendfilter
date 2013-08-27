d9dd138e47651b4a58a806b1220985d509073714
Jørgen Jacobsen <jorgeja@gmail.com>
2013-08-27 22:50:56 +0200

    Added some more functionality and styling

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -18,7 +18,16 @@ define(['jquery', 'app/fb'], function(jq,fb) {
             quizView : $('#quiz-view'),
             resultsView : $('#results-view'),
             startButton : $('#start-button'),
-            loginButton : $('#login-button')
+            loginButton : $('#login-button'),
+            question : $('#question'),
+            category : $('#category'),
+            answer : $('#answer'),
+            personOneImage : $('#img-1'),
+            personTwoImage : $('#img-2'),
+            personThreeImage : $('#img-3'),
+            personOneName : $('#name-1'),
+            personTwoName : $('#name-2'),
+            personThreeName : $('#name-3')
         },
 
         init : function () {
@@ -28,12 +37,10 @@ define(['jquery', 'app/fb'], function(jq,fb) {
         },
 
         authenticationResponse : function (response) {
-            console.log(response.authResponse);
             if(response.authResponse !== undefined) {
                 app.setLoginCredentials();
             } else {
                 fb.login(function(response) {
-                    console.log(response);
                     if(response.authResponse !== null) {
                         app.setLoginCredentials();
                     } else {
@@ -53,20 +60,50 @@ define(['jquery', 'app/fb'], function(jq,fb) {
 
         createQuiz : function () {
             var numberOfFriends   = app.gvar.friends.data.length-1,
-                selectableFriends = new Array(),
-                arbiTraryPerson;
+                selectableFriends = new Array();
 
-            selectableFriends.push(app.gvar.friends.data[app.getRandomArbitraryNumber(0,numberOfFriends)]);
+            // Get alternatives
             selectableFriends.push(app.gvar.friends.data[app.getRandomArbitraryNumber(0,numberOfFriends)]);
             selectableFriends.push(app.gvar.friends.data[app.getRandomArbitraryNumber(0,numberOfFriends)]);
 
-            arbiTraryPerson = selectableFriends[app.getRandomArbitraryNumber(0,2)];
+            /*fb.apiCall('/' + selectableFriends[0].id + '?fields=picture.type(large)', function (response) {
+                app.el.personOneImage.attr('src', response.picture.data.url); 
+                fb.apiCall('/' + selectableFriends[1].id + '?fields=picture.type(large)', function (response) {
+                    app.el.personTwoImage.attr('src', response.picture.data.url); 
+                    app.getAnswer(numberOfFriends, function (randomPerson, response) {
+                        app.el.category.text(response.category);
+                        app.el.answer.text(response.name);
+                        fb.apiCall('/' + randomPerson.id + '?fields=picture.type(large)', function (response) {
+                            app.el.personThreeImage.attr('src', response.picture.data.url); 
+                            app.el.loadingView.hide();
+                            app.el.quizView.show();
+                        });
+                    });
+                });
+            });*/
 
             app.getAnswer(numberOfFriends, function (randomPerson, response) {
-                app.el.quizView.show();
-                app.el.loadingView.hide();
-                console.log(randomPerson, response);
+                app.el.category.text(response.category);
+                app.el.answer.text(response.name);
+                randomPerson.correct = true;
+                selectableFriends.push(randomPerson);
+                selectableFriends = selectableFriends.sort(function() {return 0.5 - Math.random()}); // randomise friends
+                fb.apiCall('/' + selectableFriends[0].id + '?fields=picture.width(200).height(200).type(square)', function (response) {
+                    app.el.personOneImage.attr('src', response.picture.data.url);
+                    app.el.personOneName.text(selectableFriends[0].name);
+                    fb.apiCall('/' + selectableFriends[1].id + '?fields=picture.width(200).height(200).type(square)', function (response) {
+                        app.el.personTwoImage.attr('src', response.picture.data.url);
+                        app.el.personTwoName.text(selectableFriends[1].name);
+                        fb.apiCall('/' + selectableFriends[2].id + '?fields=picture.width(200).height(200).type(square)', function (response) {
+                            app.el.personThreeImage.attr('src', response.picture.data.url);
+                            app.el.personThreeName.text(selectableFriends[2].name);
+                            app.el.loadingView.hide();
+                            app.el.quizView.show();
+                        });
+                    });
+                });
             });
+
         },
 
         getAnswer : function (numberOfFriends, callback) {
